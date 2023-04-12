6e193ac44ba9e8ee4617d6517c2152a187170a8e
Mandorica <44854660+Mandorica@users.noreply.github.com>
2023-04-12 22:33:39 +0900

    Gameover 씬 전환 완료
    Gameover 씬 전환 완료

---
+++ Assets/09.Scripts/SceneControl/GameOverScreen.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOverScreen : MonoBehaviour
+{
+    public void Setup()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void ReplayButton()
+    {
+        SceneManager.LoadScene("mainScene");
+    }
+
+    public void QuitButton()
+    {
+        SceneManager.LoadScene("Start");
+    }
+}
