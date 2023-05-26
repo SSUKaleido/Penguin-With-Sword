0d53d7b278b7b1004a8b1f701ee9975199e982a4
Mandorica <44854660+Mandorica@users.noreply.github.com>
2023-05-26 23:41:44 +0900

    Timer scene 전환

---
+++ Assets/09.Scripts/Mananger/UIManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private GameObject gameOverText;
+    [SerializeField] private float maxTime = 60f;
+    private float timeLeft;
+    private Image timerBar;
+    void Start()
+    {
+        timerBar = GetComponent<Image>();
+        timeLeft = maxTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeLeft > 0)
+        {
+            timeLeft -= Time.deltaTime;
+            timerBar.fillAmount = timeLeft / maxTime;
+        }
+        else
+        {
+            {
+                Time.timeScale = 0;
+            }
+        }
+    }
+}
