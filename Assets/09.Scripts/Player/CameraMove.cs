548221a127fc7db384197a4eb878308341726720
TaeHyun Kim <devupkim@gmail.com>
2023-07-05 17:32:12 +0900

    Update Penguin Model and Animation
    Penguin IDLE, Walking Done

--- Assets/09.Scripts/Player/CameraMove.cs
+++ Assets/09.Scripts/Player/CameraMove.cs
@@ -4,32 +4,20 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class PlayerAnimation : MonoBehaviour
+public class CameraMove : MonoBehaviour
 {
     public GameObject camera;
     private Vector3 camPlayer;
-
-    private Animator _playerAnimator;
-    
     //private Vector3 dir = Vector3.zero;
     //public LayerMask layer;
     
     private void Start()
     {
         camPlayer = camera.transform.position - this.transform.position;
-        _playerAnimator = this.GetComponentInChildren<Animator>();
     }
 
     void Update()
     {
         camera.transform.position = this.transform.position + camPlayer;
-        
-        // _playerAnimator.Parameter변경
-        //_playerAnimator.SetFloat("ForwardVelocity", dir.magnitude);
-
-        if (Input.GetKeyDown("r"))
-        {
-            _playerAnimator.SetTrigger("Smash");
-        }
     }
 }
