29e1212c0b1ddf2e5bb8247f6a86f238ca6c8529
Mandorica <44854660+Mandorica@users.noreply.github.com>
2023-07-06 02:43:51 +0900

    Bgm, sfx 제작함

---
+++ Assets/09.Scripts/Mananger/SoundManager.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager instance;
+
+    [Header("BGM")] 
+    public AudioClip bgmClip;
+    public float bgmVolume;
+    private AudioSource bgmPlayer;
+
+    [Header("SFX")] 
+    public AudioClip[] sfxClips;
+    public float sfxVolume;
+    public int channels;
+    private AudioSource[] sfxPlayers;
+    private int channelIndex;
+
+    public enum Sfx { Angry_Penguin, FishCooking, MissionClear, Penguin_diving, penguin_smash, SleepingCaptain}
+    void Awake()
+    {
+        instance = this;
+        Init();
+    }
+
+    void Init()
+    {
+        // 배경음 플레이어 초기화
+        GameObject bgmObject = new GameObject("BgmPlayer");
+        bgmObject.transform.parent = transform;
+        bgmPlayer = bgmObject.AddComponent<AudioSource>();
+        bgmPlayer.playOnAwake = false;
+        bgmPlayer.loop = true;
+        bgmPlayer.volume = bgmVolume;
+        bgmPlayer.clip = bgmClip;
+        
+        // 효과음 플레이어 초기화
+        GameObject sfxObject = new GameObject("BgmPlayer");
+        sfxObject.transform.parent = transform;
+        sfxPlayers = new AudioSource[channels];
+
+        for (int index = 0; index < sfxPlayers.Length; index++)
+        {
+            sfxPlayers[index] = sfxObject.AddComponent<AudioSource>();
+            sfxPlayers[index].playOnAwake = false;
+            sfxPlayers[index].volume = sfxVolume;
+        }
+    }
+
+    public void PlayBgm(bool isPlay)
+    {
+        if (isPlay)
+        {
+            bgmPlayer.Play();
+        }
+        else
+        {
+            bgmPlayer.Stop();
+        }
+    }
+    public void PlaySfx(Sfx sfx)
+    {
+        for (int index = 0; index < sfxPlayers.Length; index++)
+        {
+            int loopIndex = (index + channelIndex) % sfxPlayers.Length;
+
+            if (sfxPlayers[loopIndex].isPlaying)
+            {
+                continue;
+            }
+
+            channelIndex = loopIndex;
+            sfxPlayers[loopIndex].clip = sfxClips[(int)sfx];
+            sfxPlayers[loopIndex].Play();
+            break;
+        }
+    }
+}
